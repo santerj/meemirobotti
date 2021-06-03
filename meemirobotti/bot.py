c9f99ce7aeb933c1ba5596511339bc922443870f
santeri <santeri.suomala@outlook.com>
2021-06-03 22:34:44 +0300

    wip wip added borked cache stuff

--- meemirobotti/bot.py
+++ meemirobotti/bot.py
@@ -1,4 +1,6 @@
 import typing
+import configparser
+import time
 
 from classes import Meme
 from reddit import Redditor
@@ -7,17 +9,17 @@ from telegram import TelegramError, ReplyKeyboardMarkup, ReplyKeyboardRemove, Up
 
 # TODO: check imports for unused classes and methods
 
-class TelegramBot:
+class Bot:
 
     def __init__(self) -> None:
         config = configparser.ConfigParser()
         config.read('config')
         self.token = config['telegram']['bot_token']
-
-        self.updater = Updater(token=self.token, use_context=True)
-        self.dispatcher = updater.dispatcher
         self.redditor = Redditor()
         self.meme_cache = []
+
+        updater = Updater(token=self.token, use_context=True)
+        dispatcher = updater.dispatcher
 
         self.commands = {
             "meme": self.send_meme
@@ -27,25 +29,32 @@ class TelegramBot:
             dispatcher.add_handler(PrefixHandler(['/'], cmd, callback))
             dispatcher.add_handler(CommandHandler(cmd, callback))
 
-        dispatcher.job_queue.run_repeating(self.refreshCache, interval=60, first=5)        
-        self.updater.start_polling()
+        dispatcher.job_queue.run_repeating(callback=self.refresh_cache, interval=4, first=5)
+        updater.start_polling()
+        updater.idle()
  
-
-    def refreshCache(self, update: Update) -> None:
-        while len(self.meme_cache) < 15:
-            meme = self.redditor.get_meme()
+    def refresh_cache(self, update: Update) -> None:
+        print("refresh_cache")
+        if len(self.meme_cache) < 20:
+            start = time.time()
+            meme = self.redditor.get_meme(legacy=True)
             self.meme_cache.append(meme)
+            end = time.time()
+            print(f"refreshed in: {end-start}")
 
     def send_meme(self, update: Update, context: CallbackContext) -> None:
+
         if len(self.meme_cache) > 0:
+            print("cache hit!", len(self.meme_cache))
             meme = self.meme_cache[0]
             self.meme_cache = self.meme_cache[1:]
         else:
+            print("worst case scenario")
             meme = self.redditor.get_meme()
-        
-        context.bot.send_message(chat_id=update.message.chat_id, message=
-            f"""r/{meme.sub}:
-            {meme.title}
-            {meme.link}
-            """
+
+        context.bot.send_message(chat_id=update.message.chat_id, text=
+            f"r/{meme.sub}:\n{meme.title}\n{meme.url}"
         )
+
+if __name__ == '__main__':
+    Bot()
--- meemirobotti/reddit.py
+++ meemirobotti/reddit.py
@@ -5,6 +5,7 @@ import typing
 from typing import List
 from classes import Meme
 
+import time
 
 class Redditor:
     """
@@ -42,7 +43,9 @@ class Redditor:
                 self.subreddits.append(line.rstrip("\n"))
 
     def get_meme(self, legacy: bool = False) -> Meme:
-        
+
+        start = time.time()
+
         limit = 75
         allow_pre = ("imgur", "i.imgur", "i.redd")
         allow_post = ('.jpg', 'jpeg', '.png', '.gif')
@@ -72,6 +75,18 @@ class Redditor:
             loc = len("https://")
             if post.url[loc:].startswith(allow_pre) and post.url.endswith(allow_post):
                 meme = Meme(post.subreddit, post.title, post.url)
+                end = time.time()
+                print("reddit-pass", end-start)
                 return meme
 
+        end = time.time()
+        print("reddit-fail", end-start)
         return fault_meme  # in case list is empty or nothing matches allowlists
+
+        def get_memes(self, amount: int = 1, legacy: bool = False) -> List[Meme]:
+            """
+            The redditor is very generous and sends as many memes as the cache needs
+            """
+            pass
+            # re-implemet get_meme() with a sense of how many are returned
+            # this helps with reddit API throttling re:the cache
