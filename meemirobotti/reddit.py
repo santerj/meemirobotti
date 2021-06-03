10621337299741350e7193e656aaf09d27e1aff9
santeri <santeri.suomala@outlook.com>
2021-06-03 23:38:00 +0300

    stuff

--- meemirobotti/reddit.py
+++ meemirobotti/reddit.py
@@ -44,8 +44,6 @@ class Redditor:
 
     def get_meme(self, legacy: bool = False) -> Meme:
 
-        start = time.time()
-
         limit = 75
         allow_pre = ("imgur", "i.imgur", "i.redd")
         allow_post = ('.jpg', 'jpeg', '.png', '.gif')
@@ -75,8 +73,6 @@ class Redditor:
             loc = len("https://")
             if post.url[loc:].startswith(allow_pre) and post.url.endswith(allow_post):
                 meme = Meme(post.subreddit, post.title, post.url)
-                end = time.time()
-                print("reddit-pass", end-start)
                 return meme
 
         end = time.time()
@@ -87,6 +83,43 @@ class Redditor:
             """
             The redditor is very generous and sends as many memes as the cache needs
             """
-            pass
-            # re-implemet get_meme() with a sense of how many are returned
-            # this helps with reddit API throttling re:the cache
+            limit = 75
+            allow_pre = ("imgur", "i.imgur", "i.redd")
+            allow_post = ('.jpg', 'jpeg', '.png', '.gif')
+            fault_meme = Meme("kokeile myöhemmin uudestaan", "https://imgur.com/a/MLzITmE", "Koodissa vikaa")
+
+            if legacy:
+                # older logic, get post from a randomized sub
+                sub = self.reddit.subreddit(
+                    random.choice(self.subreddits)
+                )
+                posts = sub.top(time_filter='day', limit=limit)
+                posts = [post for post in posts]
+                if len(posts) < 5:
+                    # sub is not super active, get top posts of week instead to
+                    # avoid very low quality posts
+                    posts = sub.top(time_filter='week', limit=limit)
+                    posts = [post for post in posts]
+
+            else:  # new logic, get post from big multireddit, should be
+                # more fault tolerant
+                posts = self.reddit.subreddit(self.multireddit).hot(limit=limit)
+                posts = [post for post in posts]  # from ListingGenerator to list
+            
+            random.shuffle(posts)
+
+            post_list = []
+            loc = len("https://")
+            
+            for post in posts:
+                while len(post_list) < amount:
+                    if post.url[loc:].startswith(allow_pre) and post.url.endswith(allow_post):
+                        meme = Meme(post.subreddit, post.title, post.url)
+                        post_list.append(meme)
+                else:
+                    return post_list
+            
+
+    ## TODO:
+    # get_memes is default,
+    # get_meme calls that and returns 1
