c51cebf1f5a9a8fc189331c1c19a80ff6b0f6ba8
santeri <santeri.suomala@outlook.com>
2021-06-05 18:24:29 +0300

    added uwu

--- meemirobotti/bot.py
+++ meemirobotti/bot.py
@@ -1,6 +1,8 @@
 import typing
 import configparser
 import time
+import re
+import random
 
 from classes import Meme
 from reddit import Redditor
@@ -23,7 +25,8 @@ class Bot:
         dispatcher = updater.dispatcher
 
         self.commands = {
-            "meemi": self.send_meme
+            "meemi": self.send_meme,
+            "uwu": self.uwu
         }
 
         for cmd, callback in self.commands.items():
@@ -52,6 +55,58 @@ class Bot:
         context.bot.send_message(chat_id=update.message.chat_id, text=
             f"r/{meme.sub}:\n{meme.title}\n{meme.url}"
         )
+
+    def uwu(self, update: Update, context: CallbackContext):
+        chance = 35
+        suffixes = (
+            "uwu",
+            "UwU",
+            "o_0",
+            "ﾟ ✧^w^✧ ﾟ",
+            "(´◠ω◠`)",
+            "ฅ^•ﻌ•^ฅ",
+            "(・_・;)",
+            "◖⚆ᴥ⚆◗",
+            "(ᵘﻌᵘ)",
+            "(◕‿◕✿)",
+            "(U ᵕ U❁)",
+            "(˯ ᵘ ꒳ ᵘ ˯)",
+            "( ｡ᵘ ᵕ ᵘ ｡)",
+            "(◡ w ◡)",
+            "ʕ •ᴥ•ʔ",
+            "▼・ᴥ・▼"
+        )
+        
+        if not update.message.reply_to_message:
+            context.bot.send_message(
+                chat_id=update.message.chat_id,
+                text=random.choice(suffixes)
+                )
+            return
+        
+        if update.message.reply_to_message.text:
+            s = update.message.reply_to_message.text
+        elif update.message.reply_to_message.caption:
+            s = update.message.reply_to_message.caption
+        else:
+            return
+
+        s = re.sub('R', 'W', s)
+        s = re.sub('L', 'W', s)
+        s = re.sub('r', 'w', s)
+        s = re.sub('l', 'w', s)
+
+        words = s.split(" ")
+        for i, word in enumerate(words):
+            if random.randrange(chance) == 0:
+                # add stutter
+                l = word[0]
+                word = f"{l}-{l.lower()}-{l.lower()}" + word[1:]
+                words[i] = word
+
+        s = " ".join(words) + " " + random.choice(suffixes)
+        context.bot.send_message(chat_id=update.message.chat_id, text=s)
+
 
 if __name__ == '__main__':
     Bot()
