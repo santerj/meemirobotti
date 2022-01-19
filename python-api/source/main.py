241af1aa44a77990de60aaad757e92d25fff76d3
Santeri Suomala <santeri.suomala@outlook.com>
2022-01-19 20:02:57 +0200

    rename payload

--- python-api/source/main.py
+++ python-api/source/main.py
@@ -10,14 +10,13 @@ async def healthCheck() -> str:
     return "ok"
 
 @app.get("/help", response_class=PlainTextResponse)
-async def sendHelp(payload: str) -> str:
-    return utils.help(payload)
+async def sendHelp(content: str) -> str:
+    return utils.help(content)
 
 @app.get("/uwu", response_class=PlainTextResponse)
-async def uwu(payload: str) -> str:
-    return utils.uwuify(payload)
+async def uwu(content: str) -> str:
+    return utils.uwuify(content)
 
 @app.get("/kaannos", response_class=PlainTextResponse)
-async def kaannos(payload: str) -> str:
-    return utils.misspell(payload)
-    
+async def kaannos(content: str, modifier: int = 1) -> str:
+    return utils.misspell(content, modifier)
